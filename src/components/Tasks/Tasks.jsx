1156203ad442eb8f3dcf102925ebbeee1be89da5
Dan McAuley <danmcauley86@gmail.com>
2022-01-10 18:07:33 +0000

    Added tasks and custom checkoxes

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { TasksContainer, TasksHeader, TasksParagraph } from './Tasks.styled'
+import { TasksContainer, TasksHeader, TasksParagraph, TasksList, TaskItem, TaskEditIcon } from './Tasks.styled'
+import { Icon } from '@iconify/react';
 
 const Tasks = (props) => {
   const { tasks } = props;
@@ -8,7 +9,21 @@ const Tasks = (props) => {
       <TasksHeader>Tasks</TasksHeader>
       { tasks.length === 0 ?
           <TasksParagraph>All tasks have been completed. Great job!</TasksParagraph>
-        : null
+        : 
+          <>
+            { tasks.map((task, index) => (
+                <TasksList >
+                  <TaskItem key={index} type="checkbox" />
+                  {task}
+                    {/* <input  type="checkbox" value={task} />{task} */}
+                    {/* <input  type="input" value={task} /> */}
+                    <TaskEditIcon icon="clarity:edit-line" />
+                  {/* </TaskItem> */}
+                </TasksList>
+            ))
+            }
+          <TasksParagraph>Tasks to complete: {tasks.length}</TasksParagraph>
+          </>
       }
     </TasksContainer>
   )
